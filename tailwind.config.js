05aa77cac6582ae1dbd60cbcec88fe7d46b13516
Leslie Lauw <lauw2498@live.nl>
2022-07-03 20:33:36 +0200

    Add fonts & KOIOS themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,52 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      // Default colors
+      "default-text": "#111012",
+
+      // Colors
+      pink: {
+        300: "#ff0bac",
+        500: "#d50cb6",
+        700: "#ac0ebf",
+      },
+      purple: {
+        100: "#7a6eaa",
+        300: "#7645d9",
+        500: "#820fc9",
+        700: "#5911d2",
+        900: "#280d5f",
+      },
+      blue: {
+        300: "#657795",
+        500: "#2f12dc",
+      },
+      white: {
+        300: "#f8fbfe",
+        500: "#f0f5f9",
+        700: "#ececec",
+      },
+      gray: {
+        100: "#d7d7d7",
+        200: "#bdbdbd",
+        700: "#424242",
+        900: "#262626",
+      },
+
+      // Action colors
+      action: {
+        error: "#ff0000",
+        valid: "#00ff00",
+        warning: "#fca300",
+      },
+    },
+    extend: {
+      fontFamily: {
+        heading: ["Righteous", "sans-serif"],
+        base: ["Source Sans Pro", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
